284b694f8c2fb4847a5e0987bb8957b694f87e55
oktoala <okta.yoga.syah@gmail.com>
2024-04-09 23:29:56 +0800

    Things

--- .config/fish/functions/pcs.fish
+++
@@ -1,4 +0,0 @@
-function pcs --wraps='sudo pacman -S' --description 'alias pcs sudo pacman -S'
-  sudo pacman -S $argv
-        
-end
--- .config/hypr/script/sleep.sh
+++ .config/hypr/script/sleep.sh
@@ -1,3 +1,3 @@
 swayidle -w timeout 300 'swaylock -f -c 000000' \
-            timeout 600 'systemctl suspend' \
+            timeout 300 'systemctl suspend' \
             before-sleep 'swaylock -f -c 000000' &
